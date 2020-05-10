ff97110a11b79e3f24a0b84bf3d2dd99f07e8b9e
Billy <bryant4882@gmail.com>
2020-05-11 02:55:26 +0800

    sunday

--- sketch.js
+++ sketch.js
@@ -1,4 +1,7 @@
+
 // cross pattern
+let dim;
+
 let theShader;
 var myResults = [];
 var myResults1 = [];
@@ -22,249 +25,261 @@ var particles2 = [];
 
 
 function preload(){
-  // load the shader
+// load the shader
 //  theShader = loadShader('assets/basic.vert', 'assets/basic.frag');
 }
 
 function setup() {                      //---------setup top
-  //createCanvas(400,400);
+//createCanvas(400,400);
 
 createCanvas(window.innerWidth, window.innerHeight);
 
-  cb.setConsumerKey(consumerKey, consumerSecret);
-  cb.setToken(token, tokenSecret);
+cb.setConsumerKey(consumerKey, consumerSecret);
+cb.setToken(token, tokenSecret);
 
 
-  const particlesLength = Math.floor(window.innerWidth/10);
-  const particles1Length = Math.floor(window.innerWidth);
+const particlesLength = Math.floor(window.innerWidth/4);
+const particles1Length = Math.floor(window.innerWidth);
 
-  for(let i = 0; i<particlesLength; i++){
-    particles.push(new Particle());
-  }
+for(let i = 0; i<particlesLength; i++){
+  particles.push(new Particle());
+}
 
-  // for(let i = 0; i<particles1Length; i++){
-  //    particles1.push(new Particle1());
-  // }
+// for(let i = 0; i<particles1Length; i++){
+//    particles1.push(new Particle1());
+// }
 
-    // codebird
-  var params = {
-    q: "room",
-    result_type: 'recent',
-    count: 10
-  };
+  // codebird
+var params = {
+  q: "room",
+  result_type: 'recent',
+  count: 10
+};
 
-    var params1 = {
-    q: "window",
-    result_type: 'recent',
-    count: 10
-  };
-
-
-    var params2 = {
-    q: "door",
-    result_type: 'recent',
-    count: 10
-  };
-
-    cb.__call(
-    "search_tweets",
-    params,
-    function(reply) {
-      var statuses = reply.statuses;
-      for (var i = 0; i < statuses.length; i++) {
-        var tweet = statuses[i];
-        if (!tweet.retweeted_status) {
-          //print(tweet.text);
-          //fill('#' + tweet.user.profile_background_color);
-
-          fill(255/i);
-
-          //let myResults = statuses;
-         myResults = tweet.text;
-          let words = tweet.text.split(" ");
-          console.log(words);
-          let hyphenated = words.join("-")
-         // text(tweet.text , 0, i * 120);
-        }
-      }
-      // print the max_id which helps if you want to grab pages of data
-      //print('max_id: ' + reply.search_metadata.max_id);
-
-    }
-  );
+  var params1 = {
+  q: "window",
+  result_type: 'recent',
+  count: 10
+};
 
 
-      cb.__call(
-    "search_tweets",
-    params1,
-    function(reply) {
-      var statuses = reply.statuses;
-      for (var i = 0; i < statuses.length; i++) {
-        var tweet1 = statuses[i];
-        if (!tweet1.retweeted_status) {
-
-
-          fill(255);
-
-          //let myResults = statuses;
-         myResults1 = tweet1.text;
-          //let words = tweet1.text.split(" ");
-          //console.log(words);
-          //let hyphenated = words.join("-")
-         // text(tweet.text , 0, i * 120);
-        }
-      }
-      // print the max_id which helps if you want to grab pages of data
-      //print('max_id: ' + reply.search_metadata.max_id);
-
-    }
-  );
-
+  var params2 = {
+  q: "door",
+  result_type: 'recent',
+  count: 10
+};
 
   cb.__call(
-    "search_tweets",
-    params2,
-    function(reply) {
-      var statuses = reply.statuses;
-      for (var i = 0; i < statuses.length; i++) {
-        var tweet2 = statuses[i];
-        if (!tweet2.retweeted_status) {
-          //print(tweet.text);
-          //fill('#' + tweet.user.profile_background_color);
+  "search_tweets",
+  params,
+  function(reply) {
+    var statuses = reply.statuses;
+    for (var i = 0; i < statuses.length; i++) {
+      var tweet = statuses[i];
+      if (!tweet.retweeted_status) {
+        //print(tweet.text);
+        //fill('#' + tweet.user.profile_background_color);
 
-          fill(255/i);
+        fill(255/i);
 
-          //let myResults = statuses;
-         myResults2 = tweet2.text;
-          let words = tweet2.text.split(" ");
-          //console.log(words);
-          let hyphenated = words.join("-")
-         // text(tweet.text , 0, i * 120);
-        }
+        //let myResults = statuses;
+       myResults = tweet.text;
+        let words = tweet.text.split(" ");
+        console.log(words);
+        let hyphenated = words.join("-")
+       // text(tweet.text , 0, i * 120);
       }
-      // print the max_id which helps if you want to grab pages of data
-      //print('max_id: ' + reply.search_metadata.max_id);
-
     }
-  );
-  /////////////////////////////////////////////////
-  for (var i =0; i<50; i++){
-    particles.push(particle = new Particle());
-  }
-
-  for (var q =0; q<50; q++){
-    particles.push(particle = new Particle1());
-  }
-
-  for (var i =0; i<50; i++){
-    particles.push(particle = new Particle());
-    particles.push(particle1 = new Particle1());
-    particles.push(particle2 = new Particle2());
+    // print the max_id which helps if you want to grab pages of data
+    //print('max_id: ' + reply.search_metadata.max_id);
 
   }
-  attractor = createVector(200, 200);
-  attractor1 = createVector(width-150, height-400);
-  attractor2 = createVector(mouseX, mouseY);
+);
+
+
+    cb.__call(
+  "search_tweets",
+  params1,
+  function(reply) {
+    var statuses = reply.statuses;
+    for (var i = 0; i < statuses.length; i++) {
+      var tweet1 = statuses[i];
+      if (!tweet1.retweeted_status) {
+
+
+        fill(255);
+
+        //let myResults = statuses;
+       myResults1 = tweet1.text;
+        //let words = tweet1.text.split(" ");
+        //console.log(words);
+        //let hyphenated = words.join("-")
+       // text(tweet.text , 0, i * 120);
+      }
+    }
+    // print the max_id which helps if you want to grab pages of data
+    //print('max_id: ' + reply.search_metadata.max_id);
+
+  }
+);
+
+
+cb.__call(
+  "search_tweets",
+  params2,
+  function(reply) {
+    var statuses = reply.statuses;
+    for (var i = 0; i < statuses.length; i++) {
+      var tweet2 = statuses[i];
+      if (!tweet2.retweeted_status) {
+        //print(tweet.text);
+        //fill('#' + tweet.user.profile_background_color);
+
+        fill(255/i);
+
+        //let myResults = statuses;
+       myResults2 = tweet2.text;
+        let words = tweet2.text.split(" ");
+        //console.log(words);
+        let hyphenated = words.join("-")
+       // text(tweet.text , 0, i * 120);
+      }
+    }
+    // print the max_id which helps if you want to grab pages of data
+    //print('max_id: ' + reply.search_metadata.max_id);
+
+  }
+);
+/////////////////////////////////////////////////
+// for (var i =0; i<50; i++){
+//   particles.push(particle = new Particle());
+// }
+//
+// for (var q =0; q<50; q++){
+//   particles.push(particle = new Particle1());
+// }
+
+for (var i =0; i<50; i++){
+  particles.push(particle = new Particle());
+  particles.push(particle1 = new Particle1());
+  particles.push(particle2 = new Particle2());
+
+}
+attractor = createVector(width/2, height/2);    //set cm
+xim = createVector(1500, 900);
+attractor1 = createVector(width-150, height-400);
+attractor2 = createVector(mouseX, mouseY);
 
 }                                      //---------setup
 
 function draw(){
- background(0);
- //stroke(255);
- //strokeWeight(4);
- point(attractor.x, attractor.y);        // visible attractor
+background(0);
+stroke(255);
+//strokeWeight(4);
+//drawGradient(width/2, height / 2);
 
- for (var i =0; i<particles.length; i++){
-   var particle = particles[i];
-   particle.attracted(attractor);
-   particle.attracted(attractor1);
-   particle.attracted(attractor2);
-   particle.update();
-   //particle.mouse();
-   particle.show();
+point(attractor.x, attractor.y);        // draw attractor
 
-  }
-  // for (var q =0; q<particles.length; q++){
-  //   var particle1 = particles1[q];
-  //   particle1.attracted(attractor);
-  //   particle1.update();
-  //   //particle.mouse();
-  //   particle1.show();
-  //
-  //  }
-  //
-  //  for (var u =0; u<particles.length; u++){
-  //    var particle2 = particles1[u];
-  //    particle2.attracted(attractor);
-  //    particle2.update();
-  //    //particle.mouse();
-  //    particle2.show();
-  //
-  //   }
+for (var i =0; i<particles.length; i++){
+ var particle = particles[i];
+ particle.attracted(attractor);
+ //particle.attracted(attractor1);
+ //particle.attracted(attractor2);
+ particle.update();
+ //particle.mouse();
+ particle.show();
+
+}
+// for (var q =0; q<particles.length; q++){
+//   var particle1 = particles1[q];
+//   particle1.attracted(attractor);
+//   particle1.update();
+//   //particle.mouse();
+//   particle1.show();
+//
+//  }
+//
+//  for (var u =0; u<particles.length; u++){
+//    var particle2 = particles1[u];
+//    particle2.attracted(attractor);
+//    particle2.update();
+//    //particle.mouse();
+//    particle2.show();
+//
+//   }
+// background(0,90);
+
 
 }
 
-class Particle{
+class Particle{     //particle thisone
 
-  constructor(){
-    this.pos = createVector(random(width), random(height));
-    this.vel = p5.Vector.random2D();
-    this.acc = createVector();
-    var force;
-  }
+constructor(){
+  this.pos = createVector(random(width-300, width||0, 300), random(height-150, height||0, 150 ));
+//  this.vel = p5.Vector.random2D();
+  this.vel = createVector(random(-3,3))
+  this.acc = createVector();
+  var force;
+}
 
-  update(){
-    this.pos.add(this.vel);
-    this.vel.add(this.acc);
-  }
+update(){
+  this.pos.add(this.vel);
+  this.vel.add(this.acc);
+}
 
-  show(){
+show(){
 
-      //stroke(255);
-      //strokeWeight(4);
-      //point(this.pos.x, this.pos.y);
+    //stroke(255);
+    //strokeWeight(4);
+    //point(this.pos.x, this.pos.y);
 
-      var txt = myResults ;              //scope issue
-      var txt1 = myResults1;
-      var txt2 = myResults2;
-      var tx = String(txt);
-      var tx1 = String(txt1);
-      var tx2 = String(txt2);
-      textSize(7);
-      //fill(255);
+    var txt = myResults ;              //scope issue
+    var txt1 = myResults1;
+    var txt2 = myResults2;
+    var tx = String(txt);
+    var tx1 = String(txt1);
+    var tx2 = String(txt2);
+    textSize(7);
+    //fill(255);
 
-      noStroke();
-      fill(100, 100, 215, 90);
-      text(tx, this.pos.x, this.pos.y);
-      fill(20, 100, 215, 90);
-      text(tx1, this.pos.x+400, this.pos.y-200);
-      fill(120, 200, 215, 90);
-      text(tx2, this.pos.x-501, this.pos.y+140);
+    noStroke();
+    fill(100, 100, 215, 90);
+    text(tx, this.pos.x, this.pos.y);
+    fill(20, 100, 215, 90);
+    text(tx1, this.pos.x+400, this.pos.y-200);
+    fill(120, 200, 215, 90);
+    text(tx2, this.pos.x-501, this.pos.y+140);
 
-  }
+}
 
-  attracted(target){
-    var force = p5.Vector.sub(target, this.pos);   //direction
-    var dsquared = force.magSq();
-    dsquared = constrain(dsquared, 25, 500);   //constrain itself not to be too extreme
-    var G = 4;
-    var strength = G / dsquared;
-    force.setMag(strength);
-    this.acc = force;
+attracted(target){
+  var force = p5.Vector.sub(target, this.pos);   //direction
+  var dsquared = force.magSq();
+  dsquared = constrain(dsquared, 25, 500);   //constrain itself not to be too extreme
+  var G = 44;
+  var strength = G / dsquared;
+  force.setMag(strength);
+  this.acc = force;
 
-    // if (abs(this.pos.x-mouseX)<60){
-    //   this.acc.x *= -12.03;
-    // }else{
-    //   this.acc.x = force.x;
-    // }
-    // // }else{
-    // //   this.acc.x = random(-0.02, 0.02);}
-    //  if (abs(this.pos.y-mouseY)<60){
-    //   this.acc.y *= -12.03;
-    // }else{
-    //   this.acc.y = force.y;
-    // }
-  }
+  // if (abs(this.pos.x-mouseX)<10){
+  //   this.vel.x *= -1.2;
+  // }
+  // // }else{
+  // //   this.acc.x = random(-0.02, 0.02);}
+  //  if (abs(this.pos.y-mouseY)<10){
+  //   this.vel.y *= -1.2;
+  // }
+
+  if (this.pos.x < 0 || this.pos.x >width){
+ this.vel.x *=-1;
+}
+
+
+if (this.pos.y < 0 || this.pos.y > height){
+ this.vel.y *=-1;
+}
+
+}
 
 //   mouse(){
 //  if (abs(this.pos.x-mouseX)<30){
@@ -284,62 +299,62 @@ class Particle{
 
 class Particle1{
 
-  constructor(){
-    this.pos = createVector(random(width), random(height));
-    this.vel = p5.Vector.random2D();
-    this.acc = createVector();
-    var force;
+constructor(){
+  this.pos = createVector(random(width), random(height));
+  this.vel = p5.Vector.random2D();
+  this.acc = createVector();
+  var force;
+}
+
+update(){
+  this.pos.add(this.vel);
+  this.vel.add(this.acc);
+}
+
+show(){
+
+    //stroke(255);
+    //strokeWeight(4);
+    //point(this.pos.x, this.pos.y);
+
+    var txt = myResults ;              //scope issue
+    var txt1 = myResults1;
+    var txt2 = myResults2;
+    var tx = String(txt);
+    var tx1 = String(txt1);
+    var tx2 = String(txt2);
+    textSize(7);
+    //fill(255);
+
+
+    fill(20, 100, 215, 90);
+    text(tx1, this.pos.x, this.pos.y);
+
+
+}
+
+attracted(target){
+  var force = p5.Vector.sub(target, this.pos);   //direction
+  var dsquared = force.magSq();
+  dsquared = constrain(dsquared, 25, 500);   //constrain itself not to be too extreme
+  var G = 4;
+  var strength = G / dsquared;
+  force.setMag(strength);
+  this.acc = force;
+
+  if (abs(this.pos.x-mouseX)<10){
+    this.acc.x *= -12.03;
+  }else{
+    this.acc.x = force.x;
   }
-
-  update(){
-    this.pos.add(this.vel);
-    this.vel.add(this.acc);
+  // }else{
+  //   this.acc.x = random(-0.02, 0.02);}
+   if (abs(this.pos.y-mouseY)<10){
+    this.acc.y *= -12.03;
+  }else{
+    this.acc.y = force.y;
   }
-
-  show(){
-
-      //stroke(255);
-      //strokeWeight(4);
-      //point(this.pos.x, this.pos.y);
-
-      var txt = myResults ;              //scope issue
-      var txt1 = myResults1;
-      var txt2 = myResults2;
-      var tx = String(txt);
-      var tx1 = String(txt1);
-      var tx2 = String(txt2);
-      textSize(7);
-      //fill(255);
-
-
-      fill(20, 100, 215, 90);
-      text(tx1, this.pos.x, this.pos.y);
-
-
-  }
-
-  attracted(target){
-    var force = p5.Vector.sub(target, this.pos);   //direction
-    var dsquared = force.magSq();
-    dsquared = constrain(dsquared, 25, 500);   //constrain itself not to be too extreme
-    var G = 4;
-    var strength = G / dsquared;
-    force.setMag(strength);
-    this.acc = force;
-
-    if (abs(this.pos.x-mouseX)<10){
-      this.acc.x *= -12.03;
-    }else{
-      this.acc.x = force.x;
-    }
-    // }else{
-    //   this.acc.x = random(-0.02, 0.02);}
-     if (abs(this.pos.y-mouseY)<10){
-      this.acc.y *= -12.03;
-    }else{
-      this.acc.y = force.y;
-    }
-  }
+}
 
 //   mouse(){
 //  if (abs(this.pos.x-mouseX)<30){
@@ -359,61 +374,61 @@ class Particle1{
 
 class Particle2{
 
-  constructor(){
-    this.pos = createVector(random(width), random(height));
-    this.vel = p5.Vector.random2D();
-    this.acc = createVector();
-    var force;
+constructor(){
+  this.pos = createVector(random(width), random(height));
+  this.vel = p5.Vector.random2D();
+  this.acc = createVector();
+  var force;
+}
+
+update(){
+  this.pos.add(this.vel);
+  this.vel.add(this.acc);
+}
+
+show(){
+
+    //stroke(255);
+    //strokeWeight(4);
+    //point(this.pos.x, this.pos.y);
+
+    var txt = myResults ;              //scope issue
+    var txt1 = myResults1;
+    var txt2 = myResults2;
+    var tx = String(txt);
+    var tx1 = String(txt1);
+    var tx2 = String(txt2);
+    textSize(7);
+    //fill(255);
+
+
+    fill(120, 200, 215, 90);
+    text(tx2, this.pos.x, this.pos.y);
+
+}
+
+attracted(target){
+  var force = p5.Vector.sub(target, this.pos);   //direction
+  var dsquared = force.magSq();
+  dsquared = constrain(dsquared, 25, 500);   //constrain itself not to be too extreme
+  var G = 44;
+  var strength = G / dsquared;
+  force.setMag(strength);
+  this.acc = force;
+
+  if (abs(this.pos.x-mouseX)<10){
+    this.acc.x *= -12.03;
+  }else{
+    this.acc.x = force.x;
   }
-
-  update(){
-    this.pos.add(this.vel);
-    this.vel.add(this.acc);
+  // }else{
+  //   this.acc.x = random(-0.02, 0.02);}
+   if (abs(this.pos.y-mouseY)<10){
+    this.acc.y *= -12.03;
+  }else{
+    this.acc.y = force.y;
   }
-
-  show(){
-
-      //stroke(255);
-      //strokeWeight(4);
-      //point(this.pos.x, this.pos.y);
-
-      var txt = myResults ;              //scope issue
-      var txt1 = myResults1;
-      var txt2 = myResults2;
-      var tx = String(txt);
-      var tx1 = String(txt1);
-      var tx2 = String(txt2);
-      textSize(7);
-      //fill(255);
-
-
-      fill(120, 200, 215, 90);
-      text(tx2, this.pos.x, this.pos.y);
-
-  }
-
-  attracted(target){
-    var force = p5.Vector.sub(target, this.pos);   //direction
-    var dsquared = force.magSq();
-    dsquared = constrain(dsquared, 25, 500);   //constrain itself not to be too extreme
-    var G = 4;
-    var strength = G / dsquared;
-    force.setMag(strength);
-    this.acc = force;
-
-    if (abs(this.pos.x-mouseX)<10){
-      this.acc.x *= -12.03;
-    }else{
-      this.acc.x = force.x;
-    }
-    // }else{
-    //   this.acc.x = random(-0.02, 0.02);}
-     if (abs(this.pos.y-mouseY)<10){
-      this.acc.y *= -12.03;
-    }else{
-      this.acc.y = force.y;
-    }
-  }
+}
 
 //   mouse(){
 //  if (abs(this.pos.x-mouseX)<30){
@@ -429,4 +444,16 @@ class Particle2{
 // // }
 // }
 
+}
+
+function drawGradient(x, y) {
+  let radius = width/2;
+  let h = 0;
+  for (let r = radius; r > 0;  r -=1) {
+    stroke(255, h);
+    strokeWeight(2);
+    ellipse(x, y, r, r);
+    h -=0.1;
+
+  }
 }
